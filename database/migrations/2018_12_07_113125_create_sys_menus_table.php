b20b06bf40295fe70bb779ac7847fa038f402c2f
leslieeilsel <1178273431@qq.com>
2019-08-22 18:25:15 +0800

    data: 修改数据迁移脚本配置，上传最新【系统表】数据

--- database/migrations/2018_12_07_113125_create_sys_menus_table.php
+++ database/migrations/2018_12_07_113125_create_sys_menus_table.php
@@ -17,13 +17,13 @@ class CreateSysMenusTable extends Migration
             $table->increments('id');
             $table->string('title')->comment('标题');
             $table->string('name')->comment('英文标题');
-            $table->string('component')->comment('前端组件');
+            $table->string('component')->nullable()->comment('前端组件');
             $table->integer('parent_id')->nullable()->unsigned()->comment('父级ID');
             $table->string('description')->nullable()->comment('描述');
-            $table->string('path')->nullable()->comment('地址');
+            $table->string('path')->comment('地址');
             $table->integer('link_type')->default(0)->comment('链接类型');
             $table->string('url')->nullable()->comment('第三方链接');
-            $table->string('icon', 50)->default('')->comment('icon');
+            $table->string('icon', 50)->nullable()->comment('icon');
             $table->string('target')->default('_self')->comment('链接跳转类型');
             $table->integer('enabled')->default(1)->comment('是否启用');
             $table->integer('sort')->default(0)->comment('排序值');
--- database/migrations/2019_01_14_103133_create_sys_dict_data_table.php
+++ database/migrations/2019_01_14_103133_create_sys_dict_data_table.php
@@ -15,15 +15,17 @@ class CreateSysDictDataTable extends Migration
     {
         Schema::create('sys_dict_data', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title');                            // 名称
-            $table->string('value');                            // 字段值
-            $table->unsignedInteger('dict_id');                 // 字典ID
-            $table->string('description')->nullable();          // 描述
-            $table->integer('created_user_id')->nullable();     // 创建人
-            $table->integer('updated_user_id')->nullable();     // 修改人
-            $table->integer('sort')->default(null);             // 排序值
-            $table->integer('status')->default(1);              // 是否启用
+            $table->string('title')->comment('名称');
+            $table->string('value')->comment('字段值');
+            $table->unsignedInteger('dict_id')->comment('字典ID');
+            $table->string('description')->nullable()->comment('描述');
+            $table->integer('created_user_id')->nullable()->comment('创建人');
+            $table->integer('updated_user_id')->nullable()->comment('修改人');
+            $table->integer('sort')->default(null)->comment('排序值');
+            $table->integer('status')->default(1)->comment('是否启用');
             $table->timestamps();
+            // 添加外键
+            $table->foreign('dict_id')->references('id')->on('sys_dict_categories');
         });
     }
 
