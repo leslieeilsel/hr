aa8e83bda9fab78bc55aa180d529f5da3b189799
wmin_f <1208444024@qq.com>
2019-08-23 10:28:53 +0800

    fix 修改表名 用户表中添加字段

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('name');
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('password');
+            $table->string('desc')->nullable();
+            $table->integer('department_id')->nullable();
+            $table->integer('office')->nullable();
+            $table->integer('group_id')->nullable();
+            $table->rememberToken()->nullable();
+            $table->string('ding_user_id')->nullable();
+            $table->timestamp('last_login')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
